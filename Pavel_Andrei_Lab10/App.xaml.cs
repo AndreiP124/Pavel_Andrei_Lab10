4ced2501fc2a8958c74efac4a9bc045388d6cb2b
ANDREI PAVEL <andrei.pavel2@stud.ubbcluj.ro>
2021-12-13 22:36:15 +0200

    lab10

--- Pavel_Andrei_Lab10/App.xaml.cs
+++ Pavel_Andrei_Lab10/App.xaml.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using Pavel_Andrei_Lab10.Data;
 
 namespace Pavel_Andrei_Lab10
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database==null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
         }
 
         protected override void OnStart()
